c944dbff6ead79dbd92cbbcd5bae79e27eeefee4
Ariel <cervanteslibch@gmail.com>
2023-08-01 20:27:21 -0300

    list

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,8 +1,18 @@
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import { format } from "date-fns";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
 import './list.css';
 
 const List = () => {
+    const location = useLocation();
+
+    const [destination, setDestination] = useState(location.state.location);
+    const [date, setDate] = useState(location.state.date);
+    const [options, setOptions] = useState(location.state.options);
+
+
     return (
         <div>
             <Navbar />
@@ -13,12 +23,12 @@ const List = () => {
                         <h1 className="list-search-title">Buscar</h1>
                         <div className="list-item">
                             <label htmlFor="">Destino</label>
-                            <input type="text" />
+                            <input placeholder={destination} type="text" />
                         </div>
 
                         <div className="list-item">
-                            <label htmlFor="">Llegada</label>
-                            <input type="text" />
+                            <label htmlFor="">Check In</label>
+                            <span>{`${format(date[0].startDate, "dd/MM/yyyy")} hasta ${format(date[0].endDate, "dd/MM/yyyy")}`}</span>
                         </div>
                     </div>
 
